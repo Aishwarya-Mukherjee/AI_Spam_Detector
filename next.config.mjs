18720e8ecb48823e5efe10d738df2cb61d340d68
Aishwarya-Mukherjee <aishwaryamukherjee696@gmail.com>
2026-03-25 16:08:08 +0530

    Refactor code structure for improved readability and maintainability

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  webpackDevMiddleware: (config) => {
+    return config;
   },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
 export default nextConfig
